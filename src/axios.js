da424f48907615e20876283e680babcdf433daaf
Rounak Singh <rounaksingh1694@gmail.com>
2022-06-29 15:51:17 +0530

    Deploying

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://192.168.1.174:8000/api/",
+	baseURL: "http://192.46.211.10:8000/api/",
 });
 
 export default instance;
