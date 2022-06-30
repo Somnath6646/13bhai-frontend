c909b97271d80255281a75036b6b6ee210028fa7
Rounak Singh <rounaksingh1694@gmail.com>
2022-06-30 20:47:44 +0530

    Footer left

---
+++ src/components/landing/Footer.jsx
@@ -0,0 +1,11 @@
+import "../../css/landing/LandingPage.css";
+
+export const Footer = ({ ...props }) => {
+	return (
+		<div className="footer">
+			<p className="footer-hackathon-text ">
+				Made for hashnode x linode hackathon
+			</p>
+		</div>
+	);
+};
